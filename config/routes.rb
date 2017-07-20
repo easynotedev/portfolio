0b0fe66579c691160dd2ad57eb2ca4580a3c0759
Antonin Tilaon <ktilaon@gmail.com>
2017-07-20 03:42:46 -0500

    Change video height, footer, and Logo color

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # below is the => root_path
   root 'static_pages#home'
+  # /portfolio => portfolio_path
   get '/portfolio',     to: 'static_pages#portfolio'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get portfolio" do
-    get static_pages_portfolio_url
+    get portfolio_path
     assert_response :success
   end
 
